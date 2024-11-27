97d4dcae034ff545636aaf1f9234c7eae0d4582e
Rishi Kesav <121454665+rishikesav9440@users.noreply.github.com>
2024-11-27 09:52:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use(cors({
 
 // Database connection
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: srv1339.hstgr.io,
+  user: u794390554_grf,
+  password: 7fN!+JUqAp2G,
+  database: u794390554_grf,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
