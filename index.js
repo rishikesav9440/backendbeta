e952a060915a8705619f181e8426cc83de4773b2
Rishi Kesav <121454665+rishikesav9440@users.noreply.github.com>
2025-01-12 15:31:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // Add CORS middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://grfbeta.vercel.app'],
+  origin: ['http://localhost:3000', 'https://grfbeta.vercel.app', 'https://www.getreadyfast.shop/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
